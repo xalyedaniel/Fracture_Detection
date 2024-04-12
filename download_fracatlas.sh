fd346ed39bf9de363aedd577e1c363d9c1d8161d
rithvik-doshi <81876806+rithvik-doshi@users.noreply.github.com>
2024-04-12 16:14:01 -0400

    Update download_fracatlas.sh

--- download_fracatlas.sh
+++ download_fracatlas.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pip install gdown
-gdown 1KC5G_RGfL1Ufb4hYXDPSuaKl8M-hHF3h # Stored on Google Drive for ease of access, but is publicly available on Kaggle
+gdown 1KC5G_RGfL1Ufb4hYXDPSuaKl8M-hHF3h # Stored on Google Drive for ease of access, but is publicly available too
 # mkdir FracAtlas
 unzip FracAtlas.zip -d ./
 rm FracAtlas.zip
